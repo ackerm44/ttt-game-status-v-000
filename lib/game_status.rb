d443187ede8dea937d0b41a3f2110c02cba404aa
Amanda Ackerman <ackerm44@gmail.com>
2017-10-17 14:38:26 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -17,9 +17,8 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
-    win_combination.all? do |item|
-      item == "X"
-      return win_combination
+    win_combination.each do |item|
+      puts board[item]
     end
   end
 end
