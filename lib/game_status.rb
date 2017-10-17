6b2b904d698024136d17bab9b7718ef4604947f3
Amanda Ackerman <ackerm44@gmail.com>
2017-10-17 14:06:47 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,41 +15,27 @@ WIN_COMBINATIONS = [
   [2,4,6]
 ]
 
-#def won?(board)
-#  WIN_COMBINATIONS.each do |win_combination|
-#    win_combination.each do |combination_detail|
-      #index = board[index]
-      #if position_taken?(board, index)
-#        win_index_1 = win_combination[0]
-#        win_index_2 = win_combination[1]
-#        win_index_3 = win_combination[2]
-
-#        position_1 = board[win_index_1]
-#        position_2 = board[win_index_2]
-#        position_3 = board[win_index_3]
-
-#        if (position_1 == "X" && position_2 == "X" && position_3 == "X") ||
-#        (position_1 == "O" && position_2 == "O" && position_3 == "O")
-#          return win_combination
-#        else
-#          return false
-#        end
-      #end
-#  end
-#
-
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
-    win_combination.each do |combination_detail|
-      position_1 = board[combination_detail]
-      position_2 = board[combination_detail]
-      position_3 = board[combination_detail]
-      if (position_1 == "X" && position_2 == "X" && position_3 == "X") ||
-      (position_1 == "O" && position_2 == "O" && position_3 == "O")
-        return win_combination
-      else
-        return false
-      end
-    end
+      #index = board[index]
+      #if position_taken?(board, index)
+        win_index_1 = win_combination[0]
+        win_index_2 = win_combination[1]
+        win_index_3 = win_combination[2]
+
+        position_1 = board[win_index_1]
+        position_2 = board[win_index_2]
+        position_3 = board[win_index_3]
+
+        if (position_1 == "X" && position_2 == "X" && position_3 == "X") ||
+        (position_1 == "O" && position_2 == "O" && position_3 == "O")
+          return win_combination
+        else
+          return false
+        end
+      #end
   end
 end
+
+
+
