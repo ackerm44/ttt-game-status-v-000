f5d6b28b07082bd83dd3ca3d124c787c82ee8f3d
Amanda Ackerman <ackerm44@gmail.com>
2017-10-17 14:11:36 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -27,9 +27,9 @@ def won?(board)
         position_2 = board[win_index_2]
         position_3 = board[win_index_3]
 
-        if (position_1 == "X" && position_2 == "X" && position_3 == "X")
+        if (win_combination[0] == "X" && win_combination[1] == "X" && win_combination[2] == "X")
           return win_combination
-        elsif (position_1 == "O" && position_2 == "O" && position_3 == "O")
+        elsif (win_combination[0] == "O" && win_combination[1] == "O" && win_combination[2] == "O")
           return win_combination
         else
           return false
