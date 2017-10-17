174fc4d2d1b6cff5250133f6b0a7734920a8ed55
Amanda Ackerman <ackerm44@gmail.com>
2017-10-17 14:16:09 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -22,9 +22,9 @@ def won?(board)
 
 
         if board[win_combination[0]] == "X" && board[win_combination[1]] == "X" && board[win_combination[2]] == "X"
-          return win_combination[0]
+          return true
         elsif board[win_combination[0]] == "O" && board[win_combination[1]] == "O" && board[win_combination[2]] == "O"
-          return win_combination[0]
+          return true
         else
           return false
         end
