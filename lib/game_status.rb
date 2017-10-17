e20591ba618ea37801db1f1569a88e64b546febf
Amanda Ackerman <ackerm44@gmail.com>
2017-10-17 14:45:49 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -26,8 +26,8 @@ def won?(board)
     position_3 = board[win_index_3]
 
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return win_combiation
-
+      return win_combination
+      
     end
   end
 end
