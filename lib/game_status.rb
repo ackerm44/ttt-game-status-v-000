735e63db367315264c23fe794d00860c01665c86
Amanda Ackerman <ackerm44@gmail.com>
2017-10-17 13:47:54 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,7 +20,7 @@ def won?(board)
     win_combination.each do |combination_detail|
       #index = board[index]
       #if position_taken?(board, index)
-      puts win_combination
+      puts WIN_COMBINATIONS
         position_1 = board[win_combination[0]]
         position_2 = board[win_combination[1]]
         position_3 = board[win_combination[2]]
